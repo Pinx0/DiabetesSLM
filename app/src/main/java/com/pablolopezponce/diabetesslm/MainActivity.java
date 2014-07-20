05d674b4b0f343c3f97992cea7eba7f44511b1ab
Pinx0 <plponce@gmail.com>
2014-07-20 17:41:13 +0200

    first commit

---
+++ app/src/main/java/com/pablolopezponce/diabetesslm/MainActivity.java
@@ -0,0 +1,61 @@
+package com.pablolopezponce.diabetesslm;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import es.pablolopezponce.diabetesslm.resources.MyRes;
+import es.pablolopezponce.diabetesslm.setup.SetupActivity;
+
+
+public class MainActivity extends Activity
+{
+    public SharedPreferences savedData;
+
+    private String userEmail;
+    private boolean finishedSetup;
+
+    // Test 1
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        savedData = this.getSharedPreferences(getString(R.string.shared_preferences_key), Context.MODE_PRIVATE);
+        userEmail = savedData.getString("userEmail", null);
+        finishedSetup = savedData.getBoolean("finishedSetup", false);
+
+        if(userEmail!=null && finishedSetup) {
+            // Inicializamos la actividad de men� principal porque ya tenemos lo necesario //
+            Intent startMainMenuActivity = new Intent(MainActivity.this, MainMenuActivity.class);
+            startActivity(startMainMenuActivity);
+            this.finish();
+
+        } else if(userEmail!=null && !finishedSetup) {
+
+            // Inicializamos la actividad de setup porque el user no lo acab� de rellenar //
+            Intent startSetupActivity = new Intent(MainActivity.this, SetupActivity.class);
+            startActivity(startSetupActivity);
+            this.finish();
+
+        } else {
+
+            // Inicializamos la actividad de bienvenida //
+            Intent startWelcomeActivity = new Intent(MainActivity.this, WelcomeActivity.class);
+            startActivity(startWelcomeActivity);
+            this.finish();
+        }
+
+        // Typeface - Light //
+        MyRes.lightTypeface = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+
+        // Typeface - Regular //
+        MyRes.regularTypeface = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+    }
+
+
+}
